ae917ef82ebc0467e9cd06367393d43156ae2279
Foo Guo Yi <FGY30.21@ichat.sp.edu.sg>
2022-05-16 03:02:31 +0800

    modified codes in index.js

---
+++ db.js
@@ -0,0 +1,15 @@
+/* Include to use .env file */
+require('dotenv').config();
+
+const pg = require('pg');
+
+// DATABASE_URL extracted from .env file
+const dbConfig = { connectionString: process.env.DATABASE_URL };
+
+// CONNECTION TO THE DATABASE
+const pool = new pg.Pool({
+    ...dbConfig,
+    max: process.env.MAX_CONNECTION || 5,
+});
+
+module.exports = pool;
